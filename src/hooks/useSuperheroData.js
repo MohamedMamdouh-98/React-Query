e27097ba40ca4f8192bac69a56f0d38fb06fbc7b
MohamedMamdouh-98 <miidom96@gmail.com>
2023-12-18 14:14:28 +0200

    update

--- src/hooks/useSuperheroData.js
+++ src/hooks/useSuperheroData.js
@@ -1,12 +1,24 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 
 const fetchSuperHeroDetails = (heroId) => {
   return axios.get(`http://localhost:4000/superheroes/${heroId}`);
 };
 
 const useSuperheroData = (heroId) => {
-  return useQuery(["super-hero", heroId], () => fetchSuperHeroDetails(heroId));
+  const queryClient = useQueryClient();
+  return useQuery(["super-hero", heroId], () => fetchSuperHeroDetails(heroId), {
+    initialData: () => {
+      const hero = queryClient
+        .getQueryData("super-heros")
+        ?.data?.data?.find((hero) => hero.id === parseInt(heroId));
+      if (hero) {
+        return { data: hero };
+      } else {
+        return undefined;
+      }
+    },
+  });
 };
 
 export { useSuperheroData };
